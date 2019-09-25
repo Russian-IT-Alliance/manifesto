2d040c7aaeee8f6d0d7ed3530c73af40928d113f
Kirill Saksin <k.saksin@tinkoff.ru>
2019-09-25 18:31:42 +0300

    Restricted direct creation of files in verified-signatures. Keeping signatures directory in git

--- .scripts/on-master.sh
+++ .scripts/on-master.sh
@@ -7,7 +7,7 @@ bin=$(readlink -f $(dirname $0))
 mkdir -p verified-signatures
 
 touch .failed-invites
-find signatures -type f -exec echo ${bin}/verify-and-invite.sh '{}' \; | ${bin}/lib/concurrently.sh
+find signatures -type f -not \( -name .gitkeep \) -exec echo ${bin}/verify-and-invite.sh '{}' \; | grep -v | ${bin}/lib/concurrently.sh
 
 [[ "$(cat .failed-invites | wc -l)" != '0' ]] && exit -1
 ${bin}/commit-and-push.sh
